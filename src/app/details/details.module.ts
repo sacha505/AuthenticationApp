5738d68665c4a4fed31549081887da9bf07f614b
Your Name <a.cooper8789@gmail.com>
2023-11-06 21:14:41 +0000

    added files

--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetailsComponent } from './details.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: "",
+    component: DetailsComponent
+  }
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+     RouterModule.forChild(routes)
   ]
 })
 export class DetailsModule { }
